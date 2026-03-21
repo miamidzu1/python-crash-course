8dc0dc30568607e9d916ce01a23ac27372730d18
Аня <miamidzu1@gmail.com>
2026-03-22 00:20:47 +0200

    task 4 completed

--- exercises/l1_variables_and_math/task4.py
+++ exercises/l1_variables_and_math/task4.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
 
     # Assign the values of correct types to variables a, b, c, d 
     # to make the script work without errors
-
+    a = 1
+    b = 1.2
+    c = 1+2j
+    d = "12"
     # Do not change the lines below
     assert isinstance(a, int)
     assert isinstance(b, float)
