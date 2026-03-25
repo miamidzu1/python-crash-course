a361b2b6222eb7cfc2431ca3f4925e5ca646dd7f
Аня <miamidzu1@gmail.com>
2026-03-25 21:21:18 +0200

    task 3 completed

--- exercises/l3_conditions/task3.py
+++ exercises/l3_conditions/task3.py
@@ -15,8 +15,14 @@ class Keyword(Enum):
 def word2token(word: str) -> Keyword:
     if word == "if":
         return Keyword.IF
-
-
+    elif word == "while":
+        return Keyword.WHILE
+    elif word == "for":
+        return Keyword.FOR
+    elif word == "class":
+        return Keyword.CLASS
+    else:
+        return Keyword.IDENTIFIER
 # Do not change the below's code
 if __name__ == "__main__":
     assert word2token("if") == Keyword.IF
