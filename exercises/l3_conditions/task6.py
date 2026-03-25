e547b92ba0dce43d1a5bb89025b37b969d377dbf
Аня <miamidzu1@gmail.com>
2026-03-25 21:29:04 +0200

    task 6 completed

--- exercises/l3_conditions/task6.py
+++ exercises/l3_conditions/task6.py
@@ -1,6 +1,6 @@
 # Write the body of the function to make the script work without errors
 def largest_of_three(a: int, b: int, c: int) -> int:
-    pass
+    return max(a, b, c)
 
 
 if __name__ == "__main__":
