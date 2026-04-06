954947a4f3f56fe33711a07705f2504aaff074e0
Аня <miamidzu1@gmail.com>
2026-04-06 20:44:25 +0300

    task 5 completed

--- exercises/l4_loops/task5.py
+++ exercises/l4_loops/task5.py
@@ -3,7 +3,10 @@
 #
 # Return `True` is character is present. Return `False` otherwise
 def has_char(s: str, c: str) -> bool:
-    pass
+    for char in s:
+        if char == c:
+            return True
+    return False
 
 
 # Do not change the below's code
