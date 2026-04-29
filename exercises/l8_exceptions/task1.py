c095a36f6185ee8f51aacee7787ef842ada4ce02
Аня <miamidzu1@gmail.com>
2026-04-29 15:51:48 +0300

    task 1 completed

--- exercises/l8_exceptions/task1.py
+++ exercises/l8_exceptions/task1.py
@@ -1,8 +1,11 @@
 if __name__ == "__main__":
     l = [1, 2, 0, 3]
-
     s = 0
     for i, v in enumerate(l):
+        try:
+            s += i / v
+        except ZeroDivisionError:
+            continue
         # Use try...except block to handle the error.
         # If error happens add nothing to `s`, just continue the loop
         s = i / v
