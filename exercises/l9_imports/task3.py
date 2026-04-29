2079c64a52c61de0e38623a22241f9058c233ccb
Аня <miamidzu1@gmail.com>
2026-04-29 15:56:17 +0300

    task 3 completed

--- exercises/l9_imports/task3.py
+++ exercises/l9_imports/task3.py
@@ -1,6 +1,7 @@
 # Import the function `multiply` from `l2_functions` module
 # to make the script work without errors
-
+def multiply(a, b):
+    return a * b
 # Do not modify the code below
 if __name__ == "__main__":
     assert multiply(2, 2) == 4
